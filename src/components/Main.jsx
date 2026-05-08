b67dec0b52128d4edb05ae48b593a409504d93ed
Kevin <varghesekevin11241999@gmail.com>
2026-05-07 20:00:59 -0400

    react116

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,29 +1,17 @@
 import { useContext } from "react";
 import CurrentTemperatureUnitContext from "../contexts/CurrentTemperatureUnitContext";
-
 import ItemCard from "./ItemCard";
+import WeatherCard from "./WeatherCard";
 import "../blocks/Main.css";
 
 function Main({ weatherData, clothingItems = [], onCardClick }) {
-  const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
-
-  const temp = weatherData?.temperature?.[currentTemperatureUnit];
-
   return (
     <main className="main">
       <section className="weather">
-        <h2 className="weather__temp">
-          {temp !== undefined
-            ? `${temp}°${currentTemperatureUnit}`
-            : "Loading..."}
-        </h2>
+        <WeatherCard weatherData={weatherData} />
       </section>
 
-      <p className="main__description">
-        Today is{" "}
-        {temp !== undefined ? `${temp}°${currentTemperatureUnit}` : "..."} / You
-        may want to wear:
-      </p>
+      <p className="main__description">Today is / You may want to wear:</p>
 
       <section className="cards">
         {Array.isArray(clothingItems) &&
